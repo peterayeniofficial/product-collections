6ef6ea1ca7fe1719d0a725440e9905a716b79135
Peter Ayeni <ayenicology@gmail.com>
2022-02-20 21:56:52 +0000

    feat: add pagination

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,11 +1,28 @@
-import { Box, Container, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Button, Container, Stack, Text } from '@chakra-ui/react';
 import ProductTable from '../components/ProductTable';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { getAllProducts } from '../services/api';
 
 export default function Collections() {
-  const { data, isLoading, isError } = useQuery('products', getAllProducts);
-  console.log(data);
+  const queryClient = useQueryClient();
+  const [page, setPage] = React.useState(0);
+
+  const { data, isLoading, isError, isFetching, isPreviousData } = useQuery(
+    ['products', page],
+    () => getAllProducts(page),
+    { keepPreviousData: true, staleTime: 5000 }
+  );
+
+  // Prefetch the next page!
+  React.useEffect(() => {
+    if (data?.hasMore) {
+      queryClient.prefetchQuery(['projects', page + 1], () =>
+        getAllProducts(page + 1)
+      );
+    }
+  }, [data, page]);
+
   const products = data;
   if (isLoading) {
     return <Text>Loading...</Text>;
@@ -22,6 +39,23 @@ export default function Collections() {
         py={{ base: 20, md: 36 }}
       >
         <ProductTable products={products} />
+        <Box>
+          <Box>Current Page: {page + 1}</Box>
+          <Button
+            onClick={() => setPage((old) => Math.max(old - 1, 0))}
+            disabled={page === 0}
+          >
+            Previous Page
+          </Button>{' '}
+          <Button
+            onClick={() => {
+              setPage((old) => (data?.hasMore ? old + 1 : old));
+            }}
+            disabled={isPreviousData || !data?.hasMore}
+          >
+            Next Page
+          </Button>
+        </Box>
       </Stack>
     </Container>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
-export const getAllProducts = async () => {
-  const response = await fetch(`${process.env.REACT_APP_SERVER}/products`);
+export async function getAllProducts(page: number) {
+  const response = await fetch(
+    `${process.env.REACT_APP_SERVER}/products?_page=${page}`
+  );
   if (response.status !== 200) {
     throw new Error('Error fetching products');
   }
   return response.json();
-};
+}
