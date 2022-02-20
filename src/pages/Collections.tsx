3ca1ab985e4b37fa746ccf84f572eb6cd676a255
Peter Ayeni <ayenicology@gmail.com>
2022-02-20 23:25:24 +0000

    feat: add filtering

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Box, Button, Container, Stack, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  HStack,
+  Radio,
+  RadioGroup,
+  Stack,
+  Tag,
+  TagLabel,
+  Text,
+} from '@chakra-ui/react';
 import ProductTable from '../components/ProductTable';
 import { useQuery, useQueryClient } from 'react-query';
 import { getAllProducts } from '../services/api';
@@ -7,10 +20,14 @@ import { getAllProducts } from '../services/api';
 export default function Collections() {
   const queryClient = useQueryClient();
   const [page, setPage] = React.useState(0);
+  const [subscription, setSubscription] = React.useState('');
+  const [tagsLike, setTagsLike] = React.useState('');
+
+  const tags = ['Dog', 'Cat', 'Chews', 'Formula', 'Shampoo'];
 
   const { data, isLoading, isError, isFetching, isPreviousData } = useQuery(
-    ['products', page],
-    () => getAllProducts(page),
+    ['products', page, subscription, tagsLike],
+    () => getAllProducts(page, subscription, tagsLike),
     { keepPreviousData: true, staleTime: 5000 }
   );
 
@@ -18,7 +35,7 @@ export default function Collections() {
   React.useEffect(() => {
     if (data?.hasMore) {
       queryClient.prefetchQuery(['projects', page + 1], () =>
-        getAllProducts(page + 1)
+        getAllProducts(page + 1, subscription, tagsLike)
       );
     }
   }, [data, page]);
@@ -32,31 +49,69 @@ export default function Collections() {
   }
   return (
     <Container maxW={'3xl'} mt={0} centerContent>
-      <Stack
-        as={Box}
-        textAlign="center"
-        spacing={{ base: 8, md: 14 }}
-        py={{ base: 20, md: 36 }}
-      >
-        <ProductTable products={products} />
-        <Box>
-          <Box>Current Page: {page + 1}</Box>
-          <Button
-            onClick={() => setPage((old) => Math.max(old - 1, 0))}
-            disabled={page === 0}
-          >
-            Previous Page
-          </Button>{' '}
-          <Button
-            onClick={() => {
-              setPage((old) => (data?.hasMore ? old + 1 : old));
-            }}
-            disabled={isPreviousData || !data?.hasMore}
-          >
-            Next Page
-          </Button>
+      <HStack spacing={4} w="100%">
+        <Box width={'30%'}>
+          <Text fontSize={'xl'}>Filter</Text>
+          <Box mt={4}>
+            <Text>Tags</Text>
+            {tags.map((tag) => (
+              <Tag
+                as={Button}
+                size="lg"
+                colorScheme="red"
+                borderRadius="full"
+                key={tag}
+                my={1}
+                mx={2}
+                onClick={() => setTagsLike(tag)}
+              >
+                <Avatar
+                  src="https://via.placeholder.com/300x300.png?text=Clear+Eyes+Chews"
+                  size="xs"
+                  name={tag}
+                  ml={-1}
+                  mr={2}
+                />
+                <TagLabel>{tag}</TagLabel>
+              </Tag>
+            ))}
+          </Box>
+          <Box mt={4}>
+            <Text>Subscriptions</Text>
+            <RadioGroup onChange={setSubscription} value={subscription}>
+              <Stack direction="row">
+                <Radio value="true">Yes</Radio>
+                <Radio value="false">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </Box>
         </Box>
-      </Stack>
+        <Stack
+          as={Box}
+          textAlign="center"
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <ProductTable products={products} />
+          <Box>
+            <Box>Current Page: {page + 1}</Box>
+            <Button
+              onClick={() => setPage((old) => Math.max(old - 1, 0))}
+              disabled={page === 0}
+            >
+              Previous Page
+            </Button>{' '}
+            <Button
+              onClick={() => {
+                setPage((old) => (data?.hasMore ? old + 1 : old));
+              }}
+              disabled={isPreviousData || !data?.hasMore}
+            >
+              Next Page
+            </Button>
+          </Box>
+        </Stack>
+      </HStack>
     </Container>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,10 @@
-export async function getAllProducts(page: number) {
+export async function getAllProducts(
+  page: number = 0,
+  subscription?: string,
+  tagsLike?: string
+) {
   const response = await fetch(
-    `${process.env.REACT_APP_SERVER}/products?_page=${page}`
+    `${process.env.REACT_APP_SERVER}/products?subscription=${subscription}&tags_like=${tagsLike}&_page=${page}`
   );
   if (response.status !== 200) {
     throw new Error('Error fetching products');
